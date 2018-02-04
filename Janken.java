98bd3b22d52229a4adb6359075bb855821930526
yamasy1549 <sanae@yamasy.info>
2018-02-04 20:59:21 +0900

    [WIP] 出した手表示

--- Janken.java
+++ Janken.java
@@ -3,17 +3,21 @@ import java.io.*;
 import java.net.*;
 
 // 呼び出し方
-// java Janken ホスト名 ポート番号 ユーザー名
+// java Janken ホスト名 ポート番号 ユーザー名 手
 
 class Janken {
-    Janken(String hostname, String port, String name) {
-        Player player = new Player(name);
+    Player player;
+    static int myHand;
+
+    Janken(String hostname, String port, String name, int myHand) {
+        this.player = new Player(name);
+        this.myHand = myHand;
 
         try {
             Socket sock = new Socket(hostname, Integer.parseInt(port));
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream(), "UTF-8"));
             PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
-            out.println(player.name);
+            out.println(this.player.name);
             janken(in, out);
         } catch(IOException ioe) {
             System.out.println(ioe);
@@ -21,12 +25,10 @@ class Janken {
     }
 
     static void janken(BufferedReader in, PrintWriter out) throws IOException {
-        Random random = new Random();
         String s = in.readLine();
         System.out.println("対戦相手：" + s);
 
-        int myHand = random.nextInt(3);
-        out.println(myHand);
+        out.println(this.myHand);
         if((s = in.readLine()) == null) {
             System.exit(1);
         }
@@ -35,19 +37,5 @@ class Janken {
         while((s = in.readLine()) != null) {
             System.out.println(s);
         }
-    }
-
-    public static void main(String args[]) throws IOException {
-        if(args.length < 3) System.exit(1);
-
-        String hostname = args[0];
-        String port = args[1];
-        Player player = new Player(args[2]);
-
-        Socket sock = new Socket(hostname, Integer.parseInt(port));
-        BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream(), "UTF-8"));
-        PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
-        out.println(player.name);
-        janken(in, out);
     }
 }
--- JankenView.java
+++ JankenView.java
@@ -1,28 +1,54 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-class JankenView extends JFrame implements ActionListener {
-    JTextArea textarea = new JTextArea("", 1, 10);
+class JankenView extends JFrame {
+    String name;
+    JTextArea namearea = new JTextArea("", 1, 10);
 
     JankenView() {
         setBounds(100, 100, 300, 200);
 
-        textarea.requestFocus();
+        namearea.requestFocus();
 
-        JButton button = new JButton("Start");
-        button.addActionListener(this);
+        // 手ボタン
+        JButton[] handButtons = new JButton[3];
+        handButtons[0] = new JButton("グー");
+        handButtons[1] = new JButton("チョキ");
+        handButtons[2] = new JButton("パー");
+        for(JButton button: handButtons) {
+            button.addActionListener(new handListener());
+        }
 
         JPanel panel = new JPanel();
-        panel.add(textarea);
-        panel.add(button);
+        panel.add(namearea);
+        for(JButton button: handButtons) {
+            panel.add(button);
+        }
 
         getContentPane().add(panel);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        String name = textarea.getText();
-        if(name != "") {
-            new Janken("192.168.1.3", "9999", name);
+    public class handListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String name = namearea.getText();
+            if(name == "") System.exit(1);
+            name = name;
+
+            String clickedHand = e.getActionCommand();
+            int hand = 0;
+
+            switch(clickedHand) {
+                case "グー":
+                    hand = 0;
+                    break;
+                case "チョキ":
+                    hand = 1;
+                    break;
+                case "パー":
+                    hand = 2;
+            }
+
+            new Janken("192.168.1.3", "9999", name, hand);
         }
     }
 
