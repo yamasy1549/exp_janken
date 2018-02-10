8213858f5755b25aa07d9d36a1e56024ac8993c0
Sarahサラ <sarah.teoh752@gmail.com>
2018-02-10 15:03:11 +0800

    Update Ranking.java

--- views/Ranking.java
+++ views/Ranking.java
@@ -2,11 +2,14 @@ package views;
 
 import java.awt.*;
 import java.io.*;
+import javax.imageio.*;
+import java.awt.image.*;
 import javax.swing.*;
 import javax.swing.table.*;
 import java.util.*;
 import java.util.Map.*;
 import java.util.stream.*;
+import java.io.IOException;
 import static original.Constants.*;
 
 public class Ranking extends JFrame {
@@ -27,32 +30,41 @@ public class Ranking extends JFrame {
 
 
         String[] columnNames = { "ランク", "プレーヤ", "点数" };
+        Color colour = new Color(0, 0, 0, 0);
+        //Image image = new Image("./images/BACKGROUND.png");
 
         model = new DefaultTableModel(columnNames, 0);
-        ranktable = new JTable(model);
-        ranktable.setOpaque(false);
-        
+        ranktable = new JTable(model){{
+        setOpaque(false);
+        setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {{
+            setOpaque(false);
+        }});
+    }};
+
+    this.add(new JScrollPane(ranktable) {{
+            setOpaque(false);
+            getViewport().setOpaque(false);
+        }
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            try {
+            BufferedImage background;
+            background = ImageIO.read(getClass().getResource("./images/BACKGROUND.png"));
+            g.drawImage(background, 0, 0, this);
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+
+        }});
+
         int rank = 1;
         for (Entry<String, Integer> entry: sortedEntries) {
             model.addRow(new Object[] { rank, entry.getKey(), entry.getValue() });
             rank++;
         }
 
-        JPanel rankpanel = new JPanel();
-        ImageIcon icon = new ImageIcon("../images/BACKGROUND.png");
-        JLabel background = new JLabel();
-        background.setIcon(icon);
-        rankpanel.add(ranktable);
-        rankpanel.setOpaque(false);
-        
-        this.setContentPane(background);
-        this.add(rankpanel, BorderLayout.CENTER);
         this.setSize(new Dimension(300, 200));
-        this.setVisible(true);
-    }
-
-        this.setSize(new Dimension(300, 200));
-        this.add(ranktable, BorderLayout.CENTER);
         this.setVisible(true);
     }
 
